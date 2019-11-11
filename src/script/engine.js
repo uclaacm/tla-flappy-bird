e2a53c280dce2acf10b0f9f466b80a074049504a
Matthew Fan <fanma@ucla.edu>
2019-11-11 06:51:47 -0600

    Added physics to Sprite- rename to PhysicalSprite

--- src/script/engine.js
+++ src/script/engine.js
@@ -1,20 +1,21 @@
 /* -------------------------
- * Sprite Class
+ * PhysicalSprite Class
  * -------------------------
  *
  * Manages a single entity called a "sprite"
+ * "Physical" because you can set physical properties, such as acceleration or velocity.
  * Sprites are smaller images/entities that are integrated into a larger context.
  * 
  * These sprites can move and detect collisions between them.
  * 
  * -------------------------
- * Creating your own Sprite
+ * Creating your own PhysicalSprite
  * -------------------------
  * 
- * You MUST define draw() in your subclass. In general, Sprite takes care of 
+ * You MUST define draw() in your subclass. In general, PhysicalSprite takes care of 
  * everything else so it's not recommended to overload anything.
  */
-class Sprite {
+class PhysicalSprite {
     /* 
      * CONTEXT- a 2d context from an HTML Canvas element
      */
@@ -32,7 +33,7 @@ class Sprite {
 
 
     /*
-     * POS- an integer array [xCoordinate , yCoordinate] that is the new position of the Sprite
+     * POS- an integer array [xCoordinate , yCoordinate] that is the new position of the PhysicalSprite
      * 
      * Note: 
      *      The position of the sprite is its upper left corner.
@@ -40,6 +41,34 @@ class Sprite {
      */
     setPosition = (POS) => {
         this.pos = POS;
+    }
+
+
+    /*
+     * VEL- an array [xVelocity , yVelocity] that is the new velocity of the PhysicalSprite
+     */
+    setVelocity = (VEL) => {
+        this.vel = VEL;
+    }
+
+
+    /*
+     * ACCEL- an array [xAcceleration , yAcceleration] that is the new velocity of the PhysicalSprite
+     */
+    setAcceleration = (ACCEL) => {
+        this.accel = ACCEL;
+    }
+
+
+    /*
+     * Updates velocity and acceleration in both axes using kinematics
+     */
+    updateKinematics = (dt) => {
+        this.pos[0] += (this.accel[0] / 2) * Math.pow(dt , 2) + this.vel[0] * dt;
+        this.vel[0] += this.accel[0] * dt;
+
+        this.pos[1] += (this.accel[1] / 2) * Math.pow(dt , 2) + this.vel[1] * dt;
+        this.vel[1] += this.accel[1] * dt;
     }
 
 
