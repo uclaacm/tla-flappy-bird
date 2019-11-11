9c168598ce5b17292a9fb14e0e7a777c4befc4d0
Matthew Fan <fanma@ucla.edu>
2019-11-11 06:52:36 -0600

    Remove collision testing, added jumping bird

--- src/script/game.js
+++ src/script/game.js
@@ -1,31 +1,45 @@
 class Game {
     constructor(CANVAS) {
+        this.FPS = 60;
+
         this.canvas = CANVAS;
         this.context = CANVAS.getContext("2d");
+
+        window.setInterval(this.run , 1000.0 / this.FPS);
     }
 
+
+    start = () => {
+        this.bird = new Bird(this.context);
+        this.bird.setCollidable(true);
+        this.bird.setPosition([50 , 50]);
+        this.bird.setAcceleration([0 , .05]);
+        this.bird.setVelocity([0 , -1]);
+        this.bird.setBoundingBox([20 , 20]);
+
+        window.addEventListener("keydown" , event => {
+            if(event.key === " ") {
+                this.bird.jump(-2);
+            }
+        })
+
+        this.run();
+    }
+
+
     run = () => {
-        let p1 = new Pipe(this.context);
-        let p2 = new Pipe(this.context);
+        this.p1.updateKinematics(100.0 / this.FPS);
+        this.bird.updateKinematics(100.0 / this.FPS);
 
+        this.context.clearRect(0 , 0 , this.canvas.width , this.canvas.height);
 
-        p1.setCollidable(true);
-        p2.setCollidable(true);
-        p1.setPosition([0 , 0]);
-        p1.setBoundingBox([50 , 100]);
-
-        p2.setPosition([51 , 51]);
-        p2.setBoundingBox([50 , 50]);
-
-        console.log(p1.collidesWith(p2));
-
-        p1.draw();
-        p2.draw();
+        // Draw everything after this line:
+        this.bird.draw();
     }
 }
 
 
-class Pipe extends Sprite {
+class Pipe extends PhysicalSprite {
     constructor(CONTEXT) {
         super(CONTEXT);
     }
@@ -33,5 +47,23 @@ class Pipe extends Sprite {
 
     draw = () => {
         this.context.fillRect(this.pos[0] , this.pos[1] , this.boundingBox[0] , this.boundingBox[1]);
+    }
+}
+
+
+class Bird extends PhysicalSprite {
+    constructor(CONTEXT) {
+        super(CONTEXT);
+    }
+
+
+    draw = () => {
+        this.context.fillStyle = 'orange';
+        this.context.fillRect(this.pos[0] , this.pos[1] , this.boundingBox[0] , this.boundingBox[1]);
+    }
+
+
+    jump = (accel) => {
+        this.setVelocity([0 , accel]);
     }
 }
