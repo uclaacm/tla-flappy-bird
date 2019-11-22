94b4ed9f6e5e85180546127f0327ccdf26a391a9
Matthew Fan <fanma@ucla.edu>
2019-11-21 19:13:53 -0800

    Bird now retires after hitting the ground

--- src/script/game.js
+++ src/script/game.js
@@ -5,7 +5,7 @@ class Game {
         this.canvas = CANVAS;
         this.context = CANVAS.getContext("2d");
 
-        window.setInterval(this.run , 1000.0 / this.FPS);
+        this.loopRunner = window.setInterval(this.run , 1000.0 / this.FPS);
     }
 
 
@@ -16,6 +16,11 @@ class Game {
         this.bird.setAcceleration([0 , .05]);
         this.bird.setVelocity([0 , -1]);
         this.bird.setBoundingBox([20 , 20]);
+
+        this.ground = new Ground(this.context);
+        this.ground.setCollidable(true);
+        this.ground.setPosition([0 , this.canvas.height - 20]);
+        this.ground.setBoundingBox([this.canvas.width , 20]);
 
         window.addEventListener("keydown" , event => {
             if(event.key === " ") {
@@ -32,8 +37,22 @@ class Game {
 
         this.context.clearRect(0 , 0 , this.canvas.width , this.canvas.height);
 
+
+        if(this.ground.collidesWith(this.bird)) {
+            this.gameOver();
+        }
+
+
         // Draw everything after this line:
         this.bird.draw();
+        this.ground.draw();
+    }
+
+
+    gameOver = () => {
+        window.clearInterval(this.loopRunner);
+
+        console.log("gg!");
     }
 }
 
@@ -64,5 +83,18 @@ class Bird extends PhysicalSprite {
 
     jump = (accel) => {
         this.setVelocity([0 , accel]);
+    }
+}
+
+
+class Ground extends PhysicalSprite {
+    constructor(CONTEXT) {
+        super(CONTEXT);
+    }
+
+
+    draw = () => {
+        this.context.fillStyle = 'green';
+        this.context.fillRect(this.pos[0] , this.pos[1] , this.boundingBox[0] , this.boundingBox[1]);
     }
 }
