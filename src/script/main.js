bf1332b747cdc864605086722f5326d1508c83a0
Miles Kang <56098478+kilometerskang@users.noreply.github.com>
2019-11-22 15:32:24 -0800

    Update main.js

--- src/script/main.js
+++ src/script/main.js
@@ -1,3 +1,10 @@
+// Change the image of your sprite!
+function setSprite () {
+    var img = new Image();
+    img.src = 'http://sohme.com/wp-content/uploads/2015/07/red.png';
+    return img;
+}
+
 window.onload = () => {
     let flappy = new Game(document.getElementById("game"));
 
